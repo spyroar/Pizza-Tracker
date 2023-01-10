ce502935bee8d0cb84d58ba0761d514d5f9434af
Shivpal <yadavshivpal1999@gmail.com>
2023-01-10 16:48:46 +0530

    'initial'

---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const app=express();
+const path= require('path')
+const ejs=require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const port=process.env.port ||3000;
+
+
+
+
+ app.get('/',(req,res)=>{
+       res.render('home');
+ })
+
+ //  Set Template Engine
+ 
+ app.use(expressLayout) ;
+
+ app.set('views',path.join(__dirname,'/resources/views'));
+ app.set('view engine','ejs');
+
+app.listen(port,()=>{
+
+      console.log(`Port is listening on ${port}`);
+}
+)
