108dfdbf232295f2c98216a5e0fed6275b8619dc
Shivpal <yadavshivpal1999@gmail.com>
2023-01-12 18:45:52 +0530

    make home page

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ const expressLayout = require('express-ejs-layouts');
 const port=process.env.port ||3000;
 
 
+// Set Assets
 
 
  app.get('/',(req,res)=>{
        res.render('home');
  })
 
+
+ app.use(express.static('public'));
+
+
  //  Set Template Engine
- 
  app.use(expressLayout) ;
 
  app.set('views',path.join(__dirname,'/resources/views'));
